2a0e0119f9653a0fb691fbe6249207300a5a6837
Onigbinde Seye <seyeonigbinde@gmail.com>
2021-08-08 01:10:34 +0100

    Router setup and Header Components

--- ariya-app/src/components/Header.js
+++ ariya-app/src/components/Header.js
@@ -4,15 +4,28 @@ const Header = () => {
 
 
 
-
     return (
-        <section>
+        <header>
+            <div>
+                <div>
+                    <img src="" alt=""/>
+                </div>
+                <div>
+                    <button>User Signup</button>
+                    <button>Service Provider Signup</button>
+                    <button>Login</button>
+                </div>
+            </div>
+            <div>
+                <a href="#">Catering Services </a>
+                <a href="#">Media Services </a>
+                <a href="#">Venues </a>
+                <a href="#">Entertainment </a>
+                <a href="#">Decorations </a>
+                <a href="#">Make-up Artists </a>
+            </div>
 
-            
-        </section>
-
-
-
+        </header>
     )
 }
 
