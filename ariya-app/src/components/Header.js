6d0f7bf44b676d7f4ca1b9215b3848493790493c
Onigbinde Seye <seyeonigbinde@gmail.com>
2021-08-09 11:21:00 +0100

    Responsive Design Activated

--- ariya-app/src/components/Header.js
+++ ariya-app/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
 
@@ -11,70 +11,85 @@ const Header = () => {
                 <div className="header_logo">
                     {/* <img src="" alt=""/> */}
                     <h1>ARIYA</h1>
+                    <nav className="responsive_nav">
+                        <div className="dropdown">
+                            <i className="fas fa-bars fa-2x"></i>
+                            <div className="dropdown-content">
+                                <Link href="#">Catering Services </Link>
+                                <Link href="#">Media Services  </Link>
+                                <Link href="#">Venues </Link>
+                                <Link href="#">Entertainment </Link>
+                                <Link href="#">Decorations </Link>
+                                <Link href="#">Salon Services </Link>
+                                <Link href="#">Equipment</Link>
+                                <Link href="#">Transport Services </Link>
+                            </div>
+                        </div>
+                    </nav>
                 </div>
                 <div className="header_signup">
                     <button>User Signup</button>
-                    <button>Service Provider Signup</button>
+                    <button>Vendor Signup</button>
                     <button>Login</button>
                 </div>
             </div>
-            <nav>
-                <div  className="dropdown">
-                <Link href="#">Catering Services </Link>
-                <div className="dropdown-content">
-                    <Link>Wedding Catering</Link>
-                    <Link>Catering Suppliers</Link>
+            <nav className="desktop_nav">
+                <div className="dropdown">
+                    <Link href="#">Catering Services </Link>
+                    <div className="dropdown-content">
+                        <Link>Wedding Catering</Link>
+                        <Link>Catering Suppliers</Link>
+                    </div>
                 </div>
+                <div className="dropdown">
+                    <Link href="#">Media Services  </Link>
+                    <div className="dropdown-content">
+                        <Link>Wedding photographers</Link>
+                        <Link>Wedding videographers</Link>
+                    </div>
                 </div>
-                <div  className="dropdown">
-                <Link href="#">Media Services  </Link>
-                <div className="dropdown-content">
-                    <Link>Wedding photographers</Link>
-                    <Link>Wedding Videographers</Link>
+                <div className="dropdown">
+                    <Link href="#">Venues </Link>
+                    <div className="dropdown-content">
+                        <Link>Wedding venues</Link>
+                        <Link>Party venues </Link>
+                    </div>
                 </div>
+                <div className="dropdown">
+                    <Link href="#">Entertainment </Link>
+                    <div className="dropdown-content">
+                        <Link>Wedding DJ</Link>
+                        <Link> Music bands</Link>
+                        <Link> Wedding dancers</Link>
+                    </div>
                 </div>
-                <div  className="dropdown">
-                <Link href="#">Venues </Link>
-                <div className="dropdown-content">
-                    <Link>Wedding venues</Link>
-                    <Link>Party venues </Link>
+                <div className="dropdown">
+                    <Link href="#">Decorations </Link>
+                    <div className="dropdown-content">
+                        <Link>Wedding lights</Link>
+                        <Link>Wedding florist</Link>
+                    </div>
                 </div>
+                <div className="dropdown">
+                    <Link href="#">Salon Services </Link>
+                    <div className="dropdown-content">
+                        <Link>Wedding hair stylist</Link>
+                        <Link>Wedding make-up artists</Link>
+                    </div>
                 </div>
-                <div  className="dropdown">
-                <Link href="#">Entertainment </Link>
-                <div className="dropdown-content">
-                    <Link>Wedding DJ</Link>
-                    <Link> Music bands</Link>
-                    <Link> Wedding dancers</Link>
+                <div className="dropdown">
+                    <Link href="#">Equipment</Link>
+                    <div className="dropdown-content">
+                        <Link>Wedding tents</Link>
+                        <Link>Wedding tables & chairs</Link>
+                    </div>
                 </div>
-                </div>
-                <div  className="dropdown">
-                <Link href="#">Decorations </Link>
-                <div className="dropdown-content">
-                    <Link>Wedding lights</Link>
-                    <Link>Wedding florist</Link>
-                </div>
-                </div>
-                <div  className="dropdown">
-                <Link href="#">Salon Services </Link>
-                <div className="dropdown-content">
-                    <Link>Wedding hair stylist</Link>
-                    <Link>Wedding make-up artists</Link>
-                </div>
-                </div>
-                <div  className="dropdown">
-                <Link href="#">Equipment</Link>
-                <div className="dropdown-content">
-                    <Link>Wedding tents</Link>
-                    <Link>Wedding tables & chairs</Link>
-                </div>
-                </div>
-                <div  className="dropdown">
-                <Link href="#">Transport Services </Link>
-                <div className="dropdown-content">
-                    <Link>Wedding cars</Link>
-                    <Link>Wedding buses</Link>
-                </div>
+                <div className="dropdown">
+                    <Link href="#">Transport Services </Link>
+                    <div className="dropdown-content">
+                        <Link>Wedding cars</Link>
+                        <Link>Wedding buses</Link>
+                    </div>
                 </div>
             </nav>
         </header>
--- ariya-app/src/components/Homepage.js
+++ ariya-app/src/components/Homepage.js
@@ -27,42 +27,42 @@ const Homepage = () => {
                 <h2>Search by Categories </h2>
                 <div className="categories_container">
                     <Link className="categories_list">
-                        <i class="fas fa-map-marker-alt fa-5x"></i>
+                        <i className="fas fa-map-marker-alt fa-5x"></i>
                         <h4>Venues</h4>
                         <p>Wedding Venues, Outdoor Wedding Venues, Party Venues, Banquet Halls ... </p>
                     </Link>
                     <Link className="categories_list">
-                        <i class="fas fa-camera fa-5x"></i>
+                        <i className="fas fa-camera fa-5x"></i>
                         <h4>Media</h4>
                         <p>Photographers, Wedding Photographers , Videographers, Live Streaming ...</p>
                     </Link>
                     <Link className="categories_list">
-                        <i class="fas fa-utensils fa-5x"></i>
+                        <i className="fas fa-utensils fa-5x"></i>
                         <h4>Food</h4>
                         <p>Catering Services, Wedding Catering, Wedding Cakes, Beverages ...</p>
                     </Link>
                     <Link className="categories_list">
-                        <i class="fas fa-music fa-5x"></i>
+                        <i className="fas fa-music fa-5x"></i>
                         <h4>Entertainment</h4>
                         <p>Comedians, Music Bands, Wedding Bands, DJs, Singers, Dancing Groups ...</p>
                     </Link>
                     <Link className="categories_list">
-                        <i class="fas fa-gift fa-5x"></i>
+                        <i className="fas fa-gift fa-5x"></i>
                         <h4>Decoration</h4>
                         <p>Florists, Wedding Florists , Party Decorations, Wedding Decorators, Fireworks ...</p>
                     </Link>
                     <Link className="categories_list">
-                        <i class="fas fa-cut fa-5x"></i>
+                        <i className="fas fa-cut fa-5x"></i>
                         <h4>Salon Services</h4>
                         <p>Salons, Bridal Hair Stylist, Female Dressing , Male Dressing , Makeup Services ... </p>
                     </Link>
                     <Link className="categories_list">
-                        <i class="far fa-lightbulb fa-5x"></i>
+                        <i className="far fa-lightbulb fa-5x"></i>
                         <h4>Equipment</h4>
                         <p>Outdoor Tents, Wedding Tents, Party Tent Rentals, Party Lights, Event Tables ... </p>
                     </Link>
                     <Link className="categories_list">
-                        <i class="fas fa-car fa-5x"></i>
+                        <i className="fas fa-car fa-5x"></i>
                         <h4>Transportation</h4>
                         <p>Wedding Cars, Transport Services, Limousine Services, Rent Vehicles etc</p>
                     </Link>
